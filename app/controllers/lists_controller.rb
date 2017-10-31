3f1ec45335ebc2901d3b19ca5c90a51f7c31d206
Yihan <yihan.tan@gmail.com>
2017-10-31 02:26:02 +0000

    Completed

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -4,6 +4,45 @@ class ListsController < ApplicationController
    end
    
    def create_row
+      @list = List.new
+   
+      @list.title = params[:title]
+      @list.description = params[:description]
+   
+      @list.save
+      
+      redirect_to("/lists")
+   end
+
+   def index
+      @lists = List.all
+   end
+   
+   def show
+      @list = List.find(params[:id])   
+   end
+   
+   def edit_form
+      @list = List.find(params[:id])
+   end
+   
+   def update_row
+      @list = List.find(params[:id])
+      
+      @list.title = params[:title]
+      @list.description = params[:description]
+      
+      @list.save
+      
+      redirect_to("/lists/#{@list.id}")
+      
+   end
+   
+   def destroy_row
+      @list = List.find(params[:id])
+      @list.destroy
+      
+      redirect_to("/lists")
    end
     
 end
